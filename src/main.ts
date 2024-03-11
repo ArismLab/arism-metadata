a3bb230eda9ce0e92d6eab37a33a0ffbd3c09bfb
SnowyField1906 <aswdqe1x@gmail.com>
2024-03-12 02:12:34 +0700

    update

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,15 @@
 import { NestFactory } from '@nestjs/core'
 import { ConfigService } from '@nestjs/config'
-import { Logger, ValidationPipe } from '@nestjs/common'
-
 import { AppModule } from './app.module'
 
 const bootstrap = async () => {
     const app = await NestFactory.create(AppModule)
-
-    app.enableCors(
-        {
-            origin: "*",
-            methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-            credentials: true,
-        }
-    );
-    app.useGlobalPipes(new ValidationPipe())
 
     const configService = app.get(ConfigService)
     const host = configService.get<string>('host')
     const port = configService.get<number>('port')
 
     await app.listen(port)
-    Logger.log(`🚀 Listening HTTP at ${host}:${port}`, 'HTTP')
 }
 
 bootstrap()
